d41d8a73a79a268ac5baa45b02ac3314c032709e
Shane Wesley <shane.wesley@hotmail.com>
2023-01-10 23:04:38 +0000

    RJSLP-4 | Input Fields

--- login/src/Pages/Login.js
+++ login/src/Pages/Login.js
@@ -5,9 +5,17 @@ function Login() {
         <div className="container">
             <div className="login-background">
                 <div className="login-container" >
-                    <div className="button-container">
-                        <button className="button" onClick={OnLogin}>Login</button>
-                    </div>
+                    <form className="inputs-container">
+                        <input className="text-box" type="text" name="name" placeholder="Email" />
+                        <input className="text-box" type="text" name="password" placeholder="Password" />
+                        <div className="button-container">
+                            <label className="checkbox">
+                                <input type="checkbox" name="rememberPassword" />
+                                Remember Me
+                            </label>
+                            <button onClick={OnLogin}>Login</button>
+                        </div>
+                    </form>
                     <div className="link-container">
                         <a className="link" href="/sign-up">Dont have an account?</a>
                         <a className="link" href="/forgot-password">Forgot password?</a>
